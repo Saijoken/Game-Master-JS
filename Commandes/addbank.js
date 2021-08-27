0cbe02ffb3f53aad47fba6454da0d5e554b20e1f
Atsume <mohben69800@gmail.com>
2021-08-27 15:54:12 +0200

    Update addbank.js

--- Commandes/addbank.js
+++ Commandes/addbank.js
@@ -1,14 +1,21 @@
 const db = require('quick.db')
 const Discord = require('discord.js')
 const bank = new db.table("Bank")
+const {MessageEmbed} = require('discord.js');
 
 module.exports = {
     name: 'addbank',
     execute(message, args) {
-        if (message.guild.member(message.author).hasPermission("ADMINISTRATOR")) {
+        if (message.member.permissions.has("ADMINISTRATOR")) {
             if (isNaN(args[1])) return;
             bank.add(`bank_${message.guild.id}_${message.mentions.members.first().id}`, args[1]);
-            return message.channel.send(`Vous avez donné ${args[1]} Jenny dans le compte bancaire`);
-        }
+            return message.channel.send(`Vous avez donné ${args[1]} Jenny dans le compte bancaire de ${message.mentions.members.first()}`);
+        } else {
+            const embed = new MessageEmbed()
+                .setTitle(":x: | Tu n'as pas assez de charisme pour utiliser cette commande. ")
+                .setColor('#E81224')
+                .setFooter("... C'est que pour les admins : Commande AddBank")
+            message.author.send({embeds: [embed]})
+        }  
     }
 }
